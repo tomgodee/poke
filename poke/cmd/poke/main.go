52ff28c20ee55c52768674f1a1b5478461006755
Tom <trungvu8295@gmail.com>
2020-03-13 00:02:40 +0700

    initial commit

---
+++ poke/cmd/poke/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "zxc321"
+	dbname   = "poke_development"
+)
+
+func main() {
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+
+	defer db.Close()
+
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		message := "Hello " + name
+		path := c.FullPath()
+		// c.String(http.StatusOK, "Hello %s", name)
+		c.JSON(200, gin.H{
+			"message": message,
+			"path":    path,
+		})
+	})
+
+	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
