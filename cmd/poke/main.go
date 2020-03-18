9b00bb970218e21ae7b3358c236de3944fb36779
Tom <trungvu8295@gmail.com>
2020-03-19 00:02:57 +0700

    Clean up main function

--- cmd/poke/main.go
+++ cmd/poke/main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-
 	"github.com/gin-gonic/gin"
 
 	_ "github.com/lib/pq"
@@ -18,25 +15,6 @@ const (
 )
 
 func main() {
-
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-
-	db, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		panic(err)
-	}
-
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Successfully connected!")
-
-	defer db.Close()
-
 	router := gin.Default()
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -56,16 +34,4 @@ func main() {
 	})
 
 	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
-}
-
-func createUserTable(db *sql.DB) {
-	const query = `
-		CREATE TABLE IF NOT EXISTS users (
-			id serial PRIMARY KEY,
-			age INT,
-			first_name TEXT,
-			last_name TEXT,
-			email TEXT UNIQUE NOT NULL
-		)`
-	db.Exec(query)
 }
