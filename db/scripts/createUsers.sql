af589d5215349bc733065f5634b9caf16dd47b38
Tom <trungvu8295@gmail.com>
2020-03-29 13:44:36 +0700

    add scripts for populating and deleting users

---
+++ db/scripts/createUsers.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.users(user_id, username, password, email, mood)
+VALUES
+(1, 'Trung', 'zxc', 'trung@mail.com', 'sad'),
+(2, 'Diego', 'zxc', 'diego@mail.com', 'happy'),
+(3, 'Anas', 'zxc', 'anas@mail.com', 'neutral')
